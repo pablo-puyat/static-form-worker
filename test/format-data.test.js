c90a2846f1a04a1f1745cea674eb196efdbc9965
Thomas Price <tom@cbrz.com>
2019-08-07 22:24:37 -0400

    chore: fix formatting

--- test/format-data.test.js
+++ test/format-data.test.js
@@ -1,10 +1,10 @@
 import assert from 'assert'
 import formatData from '../src/utils/format-data'
 
-describe("format data", function() {
+describe('format data', function() {
   const dataPacket = {
-    key1: "Hello",
-    key2: "Goodbye"
+    key1: 'Hello',
+    key2: 'Goodbye',
   }
   it('should convert object to string', async function() {
     const formattedData = await formatData(dataPacket)
@@ -12,6 +12,6 @@ describe("format data", function() {
   })
   it('should format the object', async function() {
     const formattedData = await formatData(dataPacket)
-    assert.strictEqual(formattedData, "key1: Hello\nkey2: Goodbye\n")
+    assert.strictEqual(formattedData, 'key1: Hello\nkey2: Goodbye\n')
   })
-});
+})
